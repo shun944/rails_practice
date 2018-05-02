1265b5a2940828b3354db452a1455638f0fec7e6
shun944 <cjgxb944@yahoo.co.jp>
2018-05-02 09:27:10 +0000

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '350fae5248da4f9e8bb077c423da823e.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @non_activated_user = users(:non_activated)
   end
   
   test "should redirect edit when logged in as wrong user" do
@@ -56,6 +57,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
+    assert_redirected_to root_url
+  end
+  
+  test "should not allow the not activated attribute" do
+    log_in_as(@non_activated_user)
+    assert_not @non_activated_user.activated?
+    get users_path
+    assert_select "a[href=?]", user_path(@non_activated_user), count: 0
+    get user_path(@non_activated_user)
     assert_redirected_to root_url
   end
 
