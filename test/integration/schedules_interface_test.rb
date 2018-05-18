4155f9026cf9f564f0bf852a91c2f86f0dc86972
shun944 <cjgxb944@yahoo.co.jp>
2018-05-18 05:45:13 +0000

    add integration test for schedules

---
+++ test/integration/schedules_interface_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SchedulesInterfaceTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "schedule interface" do
+    log_in_as(@user)
+    get for_study_path
+    assert_select 'div.pagination'
+    #無効な送信
+    assert_no_difference 'Schedule.count' do
+      post for_study_create_path, params: { schedule: { title: "",
+                                    content: "" } }
+    end
+  end
+    
+end
