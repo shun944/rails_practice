d27b093a9590102bd4fd528451e5849c16316d3f
shun944 <cjgxb944@yahoo.co.jp>
2018-05-27 07:44:41 +0000

    add good relation to Schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,6 @@
 class Schedule < ApplicationRecord
     belongs_to :user
+    has_many :goods, dependent: :destroy
     default_scope -> { order(created_at: :desc) }
     validates :user_id,    presence: true
     validates :title,      presence: true, length: { maximum: 50 }
