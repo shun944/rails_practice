0f14cd908daf502b09e4c6ca972b26a388f50278
shun944 <cjgxb944@yahoo.co.jp>
2018-05-28 00:18:42 +0000

    add iine function to goos controller

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,13 @@
 class GoodsController < ApplicationController
+  before_action :logged_in_user
+  
   def create
+    @schedule = Schedule.find(params[:schedule_id])
+    @schedule.iine(current_user)
   end
 
   def destroy
+    @schedule = Good.find(params[:id]).schedule
+    @schedule.del_iine(current_user)
   end
 end
