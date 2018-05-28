e5955e7df0138f1bcb138b1eff55b7114de17b51
shun944 <cjgxb944@yahoo.co.jp>
2018-05-28 00:13:27 +0000

    add iine function to schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,4 +6,14 @@ class Schedule < ApplicationRecord
     validates :title,      presence: true, length: { maximum: 50 }
     validates :overview,   presence: true, length: { maximum: 500 }
     validates :content,    presence: true, length: { maximum: 500 }
+    
+    #"いいね"する
+    def iine(user)
+        goods.create(user_id: user.id)
+    end
+    
+    #"いいね"を解除する
+    def del_iine(user)
+        likes.find_by(user_id: user.id).destroy
+    end
 end
